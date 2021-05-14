8ecc72f262c22d5a3eb46b19300b449861899bc3
Mark Win Panerio <mpanerio@createit.com>
2021-05-14 15:50:28 +0800

    Update cyber secury page landscape

--- docs/js/main.js
+++ docs/js/main.js
@@ -2,12 +2,47 @@
 
 jQuery(function ($) {
   var $header = $('#js-header');
+  var $masonryGrid = $('.js-masonry-grid');
+  var $categoryItem = $('.js-category-item');
+  var $tabpane = $('.category-content').find('.tab-pane');
   $(window).on('scroll', function () {
     if ($(window).scrollTop() > 50) {
       $header.addClass('is-sticky');
     } else {
       $header.removeClass('is-sticky');
     }
+  });
+
+  if ($masonryGrid.length > 0) {
+    $masonryGrid.each(function (e) {
+      var $this = $(this);
+      var msnry = new Masonry($this[0], {
+        // options
+        columnWidth: '.grid-sizer',
+        itemSelector: '.grid-item',
+        percentPosition: true
+      });
+    });
+  }
+
+  var updateMasonry = function updateMasonry() {
+    $masonryGrid.each(function (e) {
+      var $this = $(this);
+      var msnry = new Masonry($this[0], {
+        // options
+        columnWidth: '.grid-sizer',
+        itemSelector: '.grid-item',
+        percentPosition: true
+      });
+    });
+  };
+
+  $('button[data-bs-toggle="tab"]').on('shown.bs.tab', updateMasonry);
+  $categoryItem.on('click', function (e) {
+    e.preventDefault();
+    var $this = $(this);
+    $this.next('.js-category-subitem').stop().slideToggle();
+    $this.toggleClass('is-active');
   });
   $(window).on('load', function () {
     if ($(window).scrollTop() > 50) {
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,8 @@
 jQuery(function($) {
     const $header = $('#js-header');
+    const $masonryGrid = $('.js-masonry-grid');
+    const $categoryItem = $('.js-category-item');
+    const $tabpane = $('.category-content').find('.tab-pane');
 
     $(window).on('scroll', function() {
         if($(window).scrollTop() > 50) {
@@ -7,6 +10,39 @@ jQuery(function($) {
         } else {
             $header.removeClass('is-sticky');
         }
+    })
+
+    if($masonryGrid.length > 0) {
+        $masonryGrid.each(function(e) {
+            const $this = $(this);
+            let msnry = new Masonry( $this[0], {
+                // options
+                columnWidth: '.grid-sizer',
+                itemSelector: '.grid-item',
+                percentPosition: true,
+            });
+        })
+    }
+
+    const updateMasonry = function(){
+        $masonryGrid.each(function(e) {
+            const $this = $(this);
+            let msnry = new Masonry( $this[0], {
+                // options
+                columnWidth: '.grid-sizer',
+                itemSelector: '.grid-item',
+                percentPosition: true,
+            });
+        })
+    }
+
+    $('button[data-bs-toggle="tab"]').on('shown.bs.tab', updateMasonry);
+
+    $categoryItem.on('click', function(e) {
+        e.preventDefault();
+        const $this = $(this);
+        $this.next('.js-category-subitem').stop().slideToggle();
+        $this.toggleClass('is-active');
     })
 
     $(window).on('load', function() {
